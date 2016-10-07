2660ec2696a52ead46022e6c58872b503a9bda63
Beatrix <bealearnscode@gmail.com>
2016-10-07 19:02:20 +0000

    react set up

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,17 @@
-import 'babel-polyfill';
+//import 'babel-polyfill';
+var React = require("react");
+var ReactDOM = require("react-dom");
+var CheeseList = require("./components/cheese-list");
 
-console.log(`Client running in ${process.env.NODE_ENV} mode`);
+var cheeses = [
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue"
+]
+
+//console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    ReactDOM.render(<CheeseList cheeses={cheeses}/>, document.getElementById("app"));
+});
