1142757f11d4ed64126867fdff187c5f68c52ea5
Beatrix <bealearnscode@gmail.com>
2016-10-07 21:15:45 +0000

    add action exports

--- client/js/actions/cheese.js
+++ client/js/actions/cheese.js
@@ -1,10 +1,12 @@
-var CHEESE = CHEESE;
-var cheese = function(cheese){
-    return {
-        type: CHEESE,
-        payload: cheese
-    }
-}
+var fetch =require('isomorphic-fetch');
+
+// var FETCH_CHEESES = "FETCH_CHEESES";
+// var fetch_cheeses = function(cheese){
+//     return {
+//         type: FETCH_CHEESES,
+//         payload: cheese
+//     }
+// }
 
 var FETCH_CHEESES_REQUEST = "FETCH_CHEESES_REQUEST";
 var fetch_cheeses_request = function(cheese){
@@ -12,17 +14,17 @@ var fetch_cheeses_request = function(cheese){
         type: FETCH_CHEESES_REQUEST,
         payload: cheese
         
-    }
+    };
     
 };
 
 var FETCH_CHEESES_SUCCESS = "FETCH_CHEESES_SUCCESS";
-var fetch_cheeses_success = function(success){
+var fetch_cheeses_success = function(cheese){
     return {
         type: FETCH_CHEESES_SUCCESS,
         payload: cheese
         
-    }
+    };
     
 };
 
@@ -32,7 +34,7 @@ var fetch_cheeses_error = function(error){
         type: FETCH_CHEESES_ERROR,
         payload: error
         
-    }
+    };
     
 };
 
@@ -42,8 +44,8 @@ var fetch_cheeses = function(cheese){
         var url = "https://cheese-list-bealearnscode.c9users.io/cheeses";
         return fetch(url).then(function(response) {
             if (response.status < 200 || response.status >= 300) {
-                var error = new Error(response.statusText)
-                error.response = response
+                var error = new Error(response.statusText);
+                error.response = response;
                 throw error;
             }
             return response;
@@ -62,5 +64,15 @@ var fetch_cheeses = function(cheese){
                 fetch_cheeses_error(error)
             );
         });
-    }
+    };
 };
+
+
+exports.FETCH_CHEESES_REQUEST = FETCH_CHEESES_REQUEST;
+exports.fetch_cheeses_request = fetch_cheeses_request;
+
+exports.FETCH_CHEESES_SUCCESS = FETCH_CHEESES_SUCCESS;
+exports.fetch_cheeses_success = fetch_cheeses_success;
+
+exports.FETCH_CHEESES_ERROR = FETCH_CHEESES_ERROR;
+exports.fetch_cheeses_error = fetch_cheeses_error;
